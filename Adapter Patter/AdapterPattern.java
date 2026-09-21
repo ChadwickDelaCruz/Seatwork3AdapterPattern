99a43c75fdbabc8acd4a63d0f3fbb7acb669802d
Chadwick Aaron Dela Cruz <delacruzchxd@gmail.com>
2026-09-21 15:25:08 +0800

    Update AdapterPattern.java

--- Adapter Patter/AdapterPattern.java
+++ Adapter Patter/AdapterPattern.java
@@ -1,1 +1,20 @@
+public class AdapterPattern {
+
+public static void main(String[] args) {
+
+SchoolManagementApp attendanceApp =
+new AttendanceSystemAdapter(new AttendanceSystem());
+
+SchoolManagementApp gradingApp =
+new GradingSystemAdapter(new GradingSystem());
+
+SchoolManagementApp libraryApp =
+new LibrarySystemAdapter(new LibrarySystem());
+
+attendanceApp.integrateSystem();
+gradingApp.integrateSystem();
+libraryApp.integrateSystem();
+
+}
+}
 
